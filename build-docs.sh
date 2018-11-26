1e06eb205481a19bee9d4bbea09285abccfdfbba
Cormac Relf <web@cormacrelf.net>
2018-11-26 17:34:39 +1100

    add timeout to netlify build script as you cannot cancel builds

--- build-docs.sh
+++ build-docs.sh
@@ -4,6 +4,10 @@ yarn && \
 yarn doc-results && \
 (cd documentation && \
  yarn && \
- yarn build)
+ timeout 180s yarn build)
+ # sometimes create-react-app is stalling when there's a missing file
+ # netlify is incapable of cancelling builds
+ # hence
+ # timeout
 
 # output in ./documentation/build
