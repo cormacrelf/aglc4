d0420df9fce763db42eff3e412b3c56232ffdb02
Cormac Relf <web@cormacrelf.net>
2018-12-28 21:49:51 +1100

    submissions to cases, case history, fix barton v chibber

--- test/configurations/csl-m.test.js
+++ test/configurations/csl-m.test.js
@@ -2,7 +2,7 @@ module.exports = {
   csl: "./src/aglc4.csl",
   jurisdictionDirs: ["./src"],
   libraries: ["./corpus.json", "./test/csl-m/*.json"],
-  suites: ["./test/core/*.yaml", "./test/csl-m/*.yaml"],
+  suites: ["./test/core/*.yaml", "./test/csl-m/*.yaml", "./test/csl-m/*/*.yaml"],
   output: "./documentation/src/results-jurisM.json"
 };
 
