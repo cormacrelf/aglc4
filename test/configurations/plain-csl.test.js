5e3cad15b060216193418cca63f64dec8c3d47c3
Cormac Relf <web@cormacrelf.net>
2019-03-09 22:27:32 +1100

    start testing Sebastian's style on journal articles

--- test/configurations/plain-csl.test.js
+++ test/configurations/plain-csl.test.js
@@ -1,7 +1,7 @@
 module.exports = {
   csl: "./src/aglc4.csl",
   libraries: ["./test/*.json", "./test/core/*.json"],
-  suites: ["./test/core/*.yaml"],
+  suites: ["./test/core/**/*.yaml"],
   output: "./documentation/src/results-csl.json"
 }
 
